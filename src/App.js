c19ae17f221c4868733748ec715bda1bae7af90d
NandhiniVelmurugan <118614680+NandhiniVelmurugan@users.noreply.github.com>
2023-10-25 16:17:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
         
         <div className="dashboard-content">
           <Routes>
-            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/courses" element={<Courses />} />
