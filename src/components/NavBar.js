5ac61ce98d0f5bd0f79297be4e8cd6da5687fec5
NandhiniVelmurugan <118614680+NandhiniVelmurugan@users.noreply.github.com>
2023-10-25 16:17:35 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,7 +11,7 @@ function NavBar() {
         />
       </div>
       <ul className="nav-links">
-        <li><Link to="/home">Home</Link></li>
+        <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/contact">Contact</Link></li>
         <li><Link to="/courses">Courses</Link></li>
